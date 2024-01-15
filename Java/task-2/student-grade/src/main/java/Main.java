51e1a83fc18e5cb8e33bb908200b5fdeaf0aa7e5
CodedCauliflower <578337@student.belgiumcampus.ac.za>
2024-01-16 00:24:13 +0200

    Finished Task 2

--- Java/task-2/student-grade/src/main/java/Main.java
+++ Java/task-2/student-grade/src/main/java/Main.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 
-public class main {
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
